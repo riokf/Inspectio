e70154010ef3c08b50f4873a1dcd47dac780d771
riokf <riokentf@gmail.com>
2020-05-12 12:43:35 +0800

    First version for Public Repository

---
+++ Inspectio/Controllers/HomeController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImageResizer;
+using Inspectio.Models;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace Inspectio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Search(string term)
+        {
+            return RedirectToAction("Index", new { id = term });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Make sure the user selected an image file
+                if (!file.ContentType.StartsWith("image"))
+                {
+                    TempData["Message"] = "Only image files may be uploaded";
+                }
+                else
+                {
+                    try
+                    {
+                        // Save the original image in the "photos" container
+                        CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+                        CloudBlobClient client = account.CreateCloudBlobClient();
+                        CloudBlobContainer container = client.GetContainerReference("photos");
+                        CloudBlockBlob photo = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                        await photo.UploadFromStreamAsync(file.InputStream);
+
+                        // Generate a thumbnail and save it in the "thumbnails" container
+                        using (var outputStream = new MemoryStream())
+                        {
+                            file.InputStream.Seek(0L, SeekOrigin.Begin);
+                            var settings = new ResizeSettings { MaxWidth = 192 };
+                            ImageBuilder.Current.Build(file.InputStream, outputStream, settings);
+                            outputStream.Seek(0L, SeekOrigin.Begin);
+                            container = client.GetContainerReference("thumbnails");
+                            CloudBlockBlob thumbnail = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                            await thumbnail.UploadFromStreamAsync(outputStream);
+                        }
+
+                        // Submit the image to Azure's Computer Vision API
+                        ComputerVisionClient vision = new ComputerVisionClient(
+                            new ApiKeyServiceClientCredentials(ConfigurationManager.AppSettings["SubscriptionKey"]),
+                            new System.Net.Http.DelegatingHandler[] { });
+                        vision.Endpoint = ConfigurationManager.AppSettings["VisionEndpoint"];
+
+                        VisualFeatureTypes[] features = new VisualFeatureTypes[] { VisualFeatureTypes.Description };
+                        var result = await vision.AnalyzeImageAsync(photo.Uri.ToString(), features);
+
+                        // Record the image description and tags in blob metadata
+                        photo.Metadata.Add("Caption", result.Description.Captions[0].Text);
+
+                        for (int i = 0; i < result.Description.Tags.Count; i++)
+                        {
+                            string key = String.Format("Tag{0}", i);
+                            photo.Metadata.Add(key, result.Description.Tags[i]);
+                        }
+
+                        await photo.SetMetadataAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        // In case something goes wrong
+                        TempData["Message"] = ex.Message;
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+        private bool HasMatchingMetadata(CloudBlockBlob blob, string term)
+        {
+            foreach (var item in blob.Metadata)
+            {
+                if (item.Key.StartsWith("Tag") && item.Value.Equals(term, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+                else if (item.Key.StartsWith("Caption") && (item.Value.IndexOf(term, StringComparison.OrdinalIgnoreCase) >= 0))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public ActionResult Index(string id)
+        {
+            // Pass a list of blob URIs and captions in ViewBag
+            CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient client = account.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference("photos");
+            List<BlobInfo> blobs = new List<BlobInfo>();
+
+            foreach (IListBlobItem item in container.ListBlobs())
+            {
+                var blob = item as CloudBlockBlob;
+
+                if (blob != null)
+                {
+                    blob.FetchAttributes(); // Get blob metadata
+
+                    // if (String.IsNullOrEmpty(id) || HasMatchingMetadata(blob, id) || blob.Metadata["Caption"].ToLower().Contains(id))
+                    if (String.IsNullOrEmpty(id) || HasMatchingMetadata(blob, id))
+                    {
+                        var caption = blob.Metadata.ContainsKey("Caption") ? blob.Metadata["Caption"] : blob.Name;
+
+                        blobs.Add(new BlobInfo()
+                        {
+                            ImageUri = blob.Uri.ToString(),
+                            ThumbnailUri = blob.Uri.ToString().Replace("/photos/", "/thumbnails/"),
+                            Caption = caption
+                        });
+                    }
+                }
+            }
+
+            ViewBag.Blobs = blobs.ToArray();
+            ViewBag.Search = id; // Prevent search box from losing its content
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Inspectio/Scripts/index.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+$(document).ready(function () {
+    $("#upload-button").click(function () {
+        $("#upload").click();
+    });
+
+    $("#upload").change(function () {
+        $("#submit").click();
+    });
+
+    $("input[type='text']").bind('focus', function () {
+        $(this).css('background-color', '#424242');
+    });
+});
